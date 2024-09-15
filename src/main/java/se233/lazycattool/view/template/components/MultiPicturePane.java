8d4103ff53e379f68bd476dff5fce72189af41c3
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-09-16 00:08:29 +0700

    delete unnecessary test ArrayList and change setStyle of firstImgBorder

--- src/main/java/se233/lazycattool/view/template/components/MultiPicturePane.java
+++ src/main/java/se233/lazycattool/view/template/components/MultiPicturePane.java
@@ -3,11 +3,9 @@ package se233.lazycattool.view.template.components;
 import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import se233.lazycattool.Launcher;
 import se233.lazycattool.model.ImageFile;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,23 +13,13 @@ public class MultiPicturePane extends HBox {
 
     private final ImageViewURL addIcon = new ImageViewURL("assets/icons/addIcon.png", 14);
     IconWithBorder addButton = new IconWithBorder(addIcon, 11,11, 4);
-    List<String> images = Arrays.asList(
-            "assets/images/blue_dusk.png",
-            "assets/images/blue_dusk.png",
-            "assets/images/blue_dusk.png",
-            "assets/images/blue_dusk.png",
-            "assets/images/blue_dusk.png"
-            );
-
-    ArrayList<ImageFile> imageFiles;
-
 
     public MultiPicturePane(ArrayList<ImageFile> imageFiles){
         this.setSpacing(12);
 
         // add Border around first image
-        StackPane wrapFirstImg = new StackPane();
-        wrapFirstImg.setStyle("-fx-padding: 2px; -fx-border-width: 2.5px; -fx-border-color: black; -fx-border-style: solid; -fx-border-radius: 6");
+        StackPane firstImgBorder = new StackPane();
+        firstImgBorder.getStyleClass().add("first-image");
 
         // Create and collect ImageViewURL objects
         List<ImageViewURL> imageViews = imageFiles.stream()
@@ -40,11 +28,11 @@ public class MultiPicturePane extends HBox {
 
         // Apply CSS class to the first image
         if (!imageViews.isEmpty()) {
-            wrapFirstImg.getChildren().add(imageViews.getFirst());
+            firstImgBorder.getChildren().add(imageViews.getFirst());
             imageViews.removeFirst();
         }
 
-        this.getChildren().add(wrapFirstImg);
+        this.getChildren().add(firstImgBorder);
         this.getChildren().addAll(imageViews);
         this.setAlignment(Pos.CENTER_LEFT);
         this.getChildren().addAll(addButton);
